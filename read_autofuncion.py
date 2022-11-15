769e2f84316342d6da1f0247f887770faf6f8797
Pedro <ponsvpedro@gmail.com>
2022-11-15 11:49:11 +0100

    Mejorada la leyenda

--- read_autofuncion.py
+++ read_autofuncion.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 import os
+from cycler import cycler
+
+plt.rc('axes', prop_cycle=(cycler('linestyle', ['-', '--', ':', '-.'])) * 
+        cycler('color', mpl.colormaps['tab10'](np.linspace(0, 1, 7) )))
 
 folderfile = os.path.abspath("./tfolders.txt")
 
@@ -13,9 +18,10 @@ for folder in foldernames:
     print(foldername)
     phifile = f"./{foldername}/phi_0000"
     try:
-        fig, ax = plt.subplots(1,1)
         tmp = pd.read_csv(phifile, sep='\t')
+        fig, ax = plt.subplots(1,1, figsize=[7,5])
         tmp.plot(x="r", y=tmp.columns[1:], ax=ax)
+        ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
         fig.savefig(f"./{foldername}/phi.pdf")
     except Exception as e:
         print(f"{foldername}: error")
